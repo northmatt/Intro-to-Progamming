536499e89cc57a0d58a3a12d7c8d48df92d09383
Matthew North <northmatt1@gmail.com>
2019-10-30 18:11:22 -0400

    Sorting
    Comments

--- Labs/Sorting/Sorting/main.cpp
+++ Labs/Sorting/Sorting/main.cpp
@@ -9,30 +9,43 @@ using std::rand;
 
 int main() {
 	srand(time(0));
+	//int sorting
+	cout << "Before:\n";
 
 	int intTest[100]{ };
-	for (int i = 0; i < sizeof(intTest) / sizeof(*intTest); i++)
+	for (int i = 0; i < sizeof(intTest) / sizeof(*intTest); i++) {
 		intTest[i] = rand() - RAND_MAX / 2;
+		cout << intTest[i] << ", ";
+	}
 
+	cout << "\n\nAfter:\n";
 	SortI(intTest, sizeof(intTest) / sizeof(*intTest));
-
-	cout << "\n\n";
+	//char sorting
+	cout << "\n\nBefore:\n";
 
 	char charTest[100]{ };
-	for (int i = 0; i < sizeof(charTest) / sizeof(*charTest); i++)
+	for (int i = 0; i < sizeof(charTest) / sizeof(*charTest); i++) {
 		charTest[i] = 65 + rand() % 26;
+		cout << charTest[i] << ", ";
+	}
 
+	cout << "\n\nAfter:\n";
 	SortC(charTest, sizeof(charTest) / sizeof(*charTest));
-
-	cout << "\n\n";
+	//string sorting
+	cout << "\n\nBefore:\n";
 
 	string strTest[100]{ "" };
 	unsigned int maxSize{ 10 };
-	for (int y = 0; y < sizeof(strTest) / sizeof(*strTest); y++)
+	for (int y = 0; y < sizeof(strTest) / sizeof(*strTest); y++) {
 		for (unsigned int x = 0; x <= rand() % maxSize; x++)
 			strTest[y] += 65 + rand() % 26;
+		cout << strTest[y] << ", ";
+	}
 
+	cout << "\n\nAfter:\n";
 	SortS(strTest, sizeof(strTest) / sizeof(*strTest));
 
 	cout << endl;
+
+	return 0;
 }
