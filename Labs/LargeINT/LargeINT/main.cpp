82dd57aeb58e8bece66cd5ef44bb1df9f8226de6
MatthewN <northmatt1@gmail.com>
2019-11-05 22:46:33 -0500

    LargeINT
    subtraction and negative numbers
    -cant do subtraction with negative numbers and have it result in a negative number

--- Labs/LargeINT/LargeINT/main.cpp
+++ Labs/LargeINT/LargeINT/main.cpp
@@ -1,12 +1,16 @@
 #include "main.h"
 
 int main() {
-	LargeINT theNum{ {9, 9, 9, 9} };
-	LargeINT theNum2( {9, 9, 9, 9} );
+	LargeINT theNum1{ {-2, 0, 0} };
+	LargeINT theNum2( {-4, 0, 0} );
+	LargeINT theNum3( {4, 0, 0, 0} );
+	LargeINT theNum4( {2, 0} );
 
-	theNum = theNum + theNum2;
-
-	cout << "Num: " << theNum.GetNumString() << endl;
+	cout << "Num 1: " << theNum1.GetNumString() << ", Num 2: " << theNum2.GetNumString() << ", Num 3: " << theNum3.GetNumString() << ", Num 4: " << theNum4.GetNumString() << "\n" << endl;
+	cout << "Num1 + Num2: " << (theNum1 + theNum2).GetNumString() << endl;
+	cout << "Num1 - Num2: " << (theNum1 - theNum2).GetNumString() << endl;
+	cout << "Num1 + Num3: " << (theNum1 + theNum3).GetNumString() << endl;
+	cout << "Num4 - Num3: " << (theNum4 - theNum3).GetNumString() << endl;
 
 	return 0;
 }
