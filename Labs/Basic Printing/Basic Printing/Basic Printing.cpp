cb3da090f41839722b020c447653060c55fe61a3
Matthew North <northmatt1@gmail.com>
2019-11-14 18:02:47 -0500

    BasicPrinting (color test)

--- Labs/Basic Printing/Basic Printing/Basic Printing.cpp
+++ Labs/Basic Printing/Basic Printing/Basic Printing.cpp
@@ -1,10 +1,13 @@
 // Basic printing tests
 
 #include <iostream>
+#include <Windows.h>
+#include <vector>
 using std::cin;
 using std::cout;
 using std::string;
 using std::endl;
+using std::vector;
 
 int main()
 {   
@@ -13,6 +16,42 @@ int main()
 	cout << "This\nis\na\nC++\nProgram\n\n";
 	cout << "This\tis\ta\tC++\tProgram\n\n\n";
 
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	string writeString = "This is a test";
+	char* writeScreen = &writeString[0];
+
+	struct color {
+		int col;
+		size_t start;
+		size_t end;
+	};
+	vector<color> colors;
+	colors.push_back(color{20, 0, 4});
+	colors.push_back(color{20, 10, 14});
+
+	if (colors.size() == 0)
+		cout << writeScreen;
+	else {
+		size_t lastCall{ 0 };
+		for (color currentCol : colors) {
+			char* p_next_write1 = &writeScreen[lastCall];
+			cout.write(p_next_write1, currentCol.start - lastCall);
+
+			SetConsoleTextAttribute(hConsole, currentCol.col);
+
+			char* p_next_write2 = &writeScreen[currentCol.start];
+			cout.write(p_next_write2, currentCol.end - currentCol.start);
+
+			SetConsoleTextAttribute(hConsole, 7);
+
+			lastCall = currentCol.end;
+		}
+
+		char* p_next_write3 = &writeScreen[lastCall];
+		cout << p_next_write3;
+	}
+	cout << "\n" << endl;
 
 	string input;
 	int sum{0};
