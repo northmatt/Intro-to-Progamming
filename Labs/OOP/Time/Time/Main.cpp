6835078152e70c89abe7818351f13f67a2fc3ad9
MatthewN <northmatt1@gmail.com>
2020-02-04 14:49:15 -0500

    Time
    -all functions/operators work

--- Labs/OOP/Time/Time/Main.cpp
+++ Labs/OOP/Time/Time/Main.cpp
@@ -1,7 +1,7 @@
 #include "Time.h"
 
 int main() {
-	Time test1(1, 1, 1);
-	Time test2(0, 2, -1);
-	std::cout << test1 << "\n" << test2 << "\n" << test2 * 3;
+	Time test1(1, 1, 30);
+	Time test2(0, 0, 0);
+	std::cout << test1 << "\n" << test2 << "\n" << test1 / test2;
 }
